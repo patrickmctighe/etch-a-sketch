d2f4d5c6aad85ce1d71b3a21ea0c6d1a18867510
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-02-03 11:51:38 -0500

    completely-functional-just-needs-more-css

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-function createGrid(rowNum){
-    const container = document.querySelector('.container')
-    const gridSize = (rowNum * rowNum) + rowNum
-    const mod = rowNum + 1
+let color = "black";
+let click = true;
+function populateBoard(size){
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => div.remove());
+   board.style.gridTemplateColumns = `repeat(${size},1fr)`;
+   board.style.gridTemplateRows= `repeat(${size},1fr)`;
+    
+   let amount = size * size
 
-    for (let i = 1 ; i<gridSize; i++){
-        const boxes = document.createElement('div')
-        boxes.classList.add('hover')
-        
-        boxes.addEventListener('mousedown',()=>{
-            boxes.style.cssText = 'background-color:black;'
-        })
-        
-if (i % mod === 0) {
-    boxes.style.cssText = 
-    'border: 0; height: 0; width: 100%'
-}else {
-    boxes.style.cssText = 
-    "border: 1px solid black; height: 25px; width: 25px";
-  } 
+   for (let i = 0 ; i< amount; i++){
+   var square = document.createElement("div");
+   square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor= 'white';
+    board.insertAdjacentElement("beforeend",square);
+       }
+    }   
+      
 
 
-    container.appendChild(boxes)
-}}
+   populateBoard(16);
 
-function addBlack ()
-    createGrid(16)
-   
-   
+   function changeSize(input){
+    if ( input < 0 || input > 100){
+        console.log("number too high or low!")
+    } else {
+        populateBoard(input)
+   };}
+
+   function colorSquare(){
+    if (click){
+    if(color === 'rainbow'){
+        this.style.backgroundColor= `hsl(${Math.random() * 360}, 100%,50%)`;
+    } else {
+    this.style.backgroundColor = color;
+   }}}
+
+   function changeColor(choice){
+    color = choice
+   }
+
+   function resetBoard(){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+   }
+
+   document.querySelector("body").addEventListener("click", (e) => {
+    if(e.target.tagName != 'BUTTON'){
+    click= !click;
+    if (click) {
+        document.querySelector(".mode").textContent = "Status: Ready to draw";
+    } else{
+        document.querySelector(".mode").textContent = "Status: Click to draw!";
+    }
+    }
+   })
